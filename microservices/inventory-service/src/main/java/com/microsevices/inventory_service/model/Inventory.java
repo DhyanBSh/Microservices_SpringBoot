03b381b466a2eca10989997343fce08ef82ae76a
DhyanBSh <39-bse-0001@kdu.ac.lk>
2025-07-22 17:26:04 +0530

    Services_Update

--- microservices/inventory-service/src/main/java/com/microsevices/inventory_service/model/Inventory.java
+++ microservices/inventory-service/src/main/java/com/microsevices/inventory_service/model/Inventory.java
@@ -1,15 +1,11 @@
 package com.microsevices.inventory_service.model;
 
-import org.springframework.data.annotation.Id;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Entity
 @Table(name = "t_inventory")
@@ -21,7 +17,6 @@ public class Inventory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    
     private Long id;
     private String skuCode;
     private Integer quantity;
--- microservices/order-service/src/main/java/com/microsevices/order_service/service/OrderService.java
+++ microservices/order-service/src/main/java/com/microsevices/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.microsevices.order_service.service;
 
+import com.microsevices.order_service.dto.InventoryResponse;
 import com.microsevices.order_service.dto.OrderLineItemsDto;
 import com.microsevices.order_service.dto.OrderRequest;
 import com.microsevices.order_service.model.Order;
@@ -8,7 +9,10 @@ import com.microsevices.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,26 +21,55 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
-        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
-                .stream()
-                .map(this::mapToDto)
-                .toList();
+//        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+//                .stream()
+//                .map(this::mapToDto)
+//                .toList();
+
+        List<OrderLineItems> orderLineItems = new ArrayList<>(
+                orderRequest.getOrderLineItemsDtoList()
+                        .stream()
+                        .map(this::mapToDto)
+                        .toList()
+        );
+
 
         order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        InventoryResponse[] inventoryResponsArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                        .retrieve()
+                                .bodyToMono(InventoryResponse[].class)
+                                        .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock) {
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is out of stock. Try again later.");
+        }
 
         orderRepository.save(order);
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
-        OrderLineItems orderLineItems = new OrderLineItems();
-        orderLineItems.setPrice(orderLineItemsDto.getPrice());
-        orderLineItems.setQuantity(orderLineItems.getQuantity());
-        orderLineItems.setSkuCode(orderLineItems.getSkuCode());
-        return orderLineItems;
+        OrderLineItems items = new OrderLineItems();
+        items.setPrice(orderLineItemsDto.getPrice());
+        items.setQuantity(orderLineItemsDto.getQuantity());
+        items.setSkuCode(orderLineItemsDto.getSkuCode());
+        return items;
     }
 }
